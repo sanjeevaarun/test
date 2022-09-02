b88b7110876e51742c4185ce95499eef4ebee580
Arun Sanjeeva <sanjeevaarun@airtory.com>
2022-09-02 18:41:49 +0530

    added game

--- dice.py
+++ dice.py
@@ -8,6 +8,18 @@ def dice_roll():
     return random.randint(1, 6)
 
 
-print(dice_roll())
+def play_game():
+    player_1 = dice_roll()
+    player_2 = dice_roll()
 
-print("Third commit!!")
+
+    print("Player 1 got: {} and Player 2 got: {}".format(player_1, player_2))
+
+    if player_1 == player_2:
+        print("It's a Draw!!")
+    elif player_1 > player_2:
+        print("Player 1 wins this round!")
+    else:
+        print("Player 2 wins this round!")
+
+play_game()
